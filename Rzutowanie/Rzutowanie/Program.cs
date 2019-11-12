2f7c94dae1bd31122be414382a5e0080cc62fc24
Bartosz <44466149+Cwiekson@users.noreply.github.com>
2019-11-12 23:57:15 +0100

    Add files via upload

---
+++ Rzutowanie/Rzutowanie/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rzutowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass myObj = new BaseClass();
+            DerrivedClass derObj = (DerrivedClass)myObj;
+            NextDerrivedClass NxtObj1 = (NextDerrivedClass)myObj;
+
+        }
+    }
+}
